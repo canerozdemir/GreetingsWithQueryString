e9959606e82d6d35e2af25782cedcab2aa58c28f
Caner <ozdemircnr@gmail.com>
2023-10-13 00:31:17 +0300

    my commit

---
+++ GreetingsWithQueryString/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using GreetingsWithQueryString.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace GreetingsWithQueryString.Controllers
+{
+    public class HomeController : Controller
+    {
+        /*private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }*/
+
+        public IActionResult Karsila(string isim, string cinsiyet)
+        {
+            ViewBag.Isim = isim;
+            ViewBag.Cinsiyet = (cinsiyet == "erkek") ? "Bey" : (cinsiyet == "kadın") ? "Hanım" : "";
+            return View();
+        }
+
+        public IActionResult Ugurla(string isim, string cinsiyet)
+        {
+            ViewBag.Isim = isim;
+            ViewBag.Cinsiyet = (cinsiyet == "erkek") ? "Bey" : (cinsiyet == "kadın") ? "Hanım" : "";
+            return View();
+        }
+
+        public IActionResult ResimGoster(int photoId, int count)
+        {
+            ViewBag.ResimId = photoId;
+            ViewBag.ResimSayisi = count;
+            return View();
+        }
+    }
+}
---
+++ GreetingsWithQueryString/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GreetingsWithQueryString.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
